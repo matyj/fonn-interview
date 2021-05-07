869881a51b1b77ad6fb6e8ead2dfbad7563d7705
wmatyjanomtek <w.matyja@nomtek.com>
2021-05-07 16:38:38 +0200

    create event implemented

--- src/events/services/impl/events-mock.service.spec.ts
+++ src/events/services/impl/events-mock.service.spec.ts
@@ -1,6 +1,9 @@
+import * as _ from 'lodash';
 import { EventsService } from '../events.service';
 import { EventsMockService } from './events-mock.service';
 import { EventsMockData } from '../../mock-data/event';
+import { BusinessError } from '../../../errors/BusinessError';
+import { DateTime } from '../../../utils/DateTime';
 
 describe('EventsMockService', () => {
   let eventsService: EventsService;
@@ -13,6 +16,64 @@ describe('EventsMockService', () => {
     it('is defined of type function', () => {
       expect(eventsService.createEvent).toBeDefined();
       expect(typeof eventsService.createEvent).toBe('function');
+    });
+
+    it('should add an event', async () => {
+      const lastEvent = getLastEvent();
+
+      const event = await eventsService.createEvent(
+        new DateTime(lastEvent.startDate).addDays(1).toString(),
+        new DateTime(lastEvent.startDate).addDays(1).toString(),
+        'The Event',
+      );
+
+      const createdEvent = await eventsService.getEvent(event.id);
+
+      expect(createdEvent.id).toBeDefined();
+    });
+
+    it('should add an when the previous ones date is the same', async () => {
+      const lastEvent = getLastEvent();
+
+      const event = await eventsService.createEvent(
+        lastEvent.startDate,
+        new DateTime(lastEvent.startDate).addDays(1).toString(),
+        'The Event',
+      );
+
+      const createdEvent = await eventsService.getEvent(event.id);
+
+      expect(createdEvent.id).toBeDefined();
+    });
+
+    it('should throw when the previous ones date is older then the new start date', async () => {
+      const lastEvent = getLastEvent();
+
+      await expect(async () => {
+        eventsService.createEvent(
+          new DateTime(lastEvent.startDate).addDays(-1).toString(),
+          new DateTime(lastEvent.startDate).addDays(1).toString(),
+          'The Event',
+        );
+      }).rejects.toThrowError(new BusinessError('Date from is younger then the last events end date'));
+    });
+
+    it('should throw error if date from is not iso', async () => {
+      await expect(async () => {
+        eventsService.createEvent('2021-05-kk', '2021-05-06', 'The Event');
+      }).rejects.toThrowError(new BusinessError('Date from is not in ISO format'));
+    });
+
+    it('should throw error if date to is not iso', async () => {
+      await expect(async () => {
+        eventsService.createEvent('2021-05-05', '2021-05-hh', 'The Event');
+      }).rejects.toThrowError(new BusinessError('Date to is not in ISO format'));
+    });
+
+    it('should throw error if date to is younger then date from', async () => {
+      await expect(async () => {
+        eventsService.createEvent('2021-05-06', '2021-05-05', 'The Event');
+      }).rejects.toThrowError(new BusinessError('Date to cannot be younger than date from'));
     });
   });
 
@@ -36,4 +97,9 @@ describe('EventsMockService', () => {
       expect(typeof eventsService.removeEvent).toBe('function');
     });
   });
+
+  const getLastEvent = () =>
+    _(EventsMockData)
+      .sortBy((item) => new Date(item.endDate))
+      .last();
 });
--- src/events/services/impl/events-mock.service.ts
+++ src/events/services/impl/events-mock.service.ts
@@ -1,26 +1,55 @@
+import * as _ from 'lodash';
 import { EventsService } from '../events.service';
 import { Event } from '../../models/event';
+import { DateValidator } from '../../../utils/DateFormatValidator';
+import { BusinessError } from '../../../errors/BusinessError';
+import { DateTime } from '../../../utils/DateTime';
 
 /* eslint-disable */
 export class EventsMockService implements EventsService {
-  constructor(private _events: Event[]) {
-  }
+  constructor(private _events: Event[]) {}
 
   createEvent(dateFrom: string, dateTo: string, title: string): Promise<Event> {
-    // @ts-ignore
-    return Promise.resolve({}); // todo: implement method
+    this.validateParams(dateFrom, dateTo);
+
+    const newEvent = { id: 'id', startDate: dateFrom, endDate: dateTo, title };
+    this._events.push(newEvent);
+
+    return Promise.resolve(newEvent);
+  }
+
+  private validateParams(dateFrom: string, dateTo: string) {
+    if (!DateValidator.isISO(dateFrom)) {
+      throw new BusinessError('Date from is not in ISO format');
+    }
+
+    if (!DateValidator.isISO(dateTo)) {
+      throw new BusinessError('Date to is not in ISO format');
+    }
+
+    if (new DateTime(dateFrom).isAfter(dateTo)) {
+      throw new BusinessError('Date to cannot be younger than date from');
+    }
+
+    const lastEvent = _(this._events)
+      .sortBy((item) => new Date(item.endDate))
+      .last();
+
+    if (new DateTime(lastEvent.startDate).isAfter(dateFrom)) {
+      throw new BusinessError('Date from is younger then the last events end date');
+    }
   }
 
   getEvent(id: string): Promise<Event> {
     // @ts-ignore
-    return Promise.resolve({}); // todo: implement method
+    return Promise.resolve(this._events.find((event) => event.id === id)); // todo: implement method
   }
 
   getEvents(
     dateFrom: string,
     dateTo: string,
     offset: number,
-    limit: number
+    limit: number,
   ): Promise<{ totalCount: number; events: Event[] }> {
     // @ts-ignore
     return Promise.resolve({}); // todo: implement method
